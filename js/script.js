19266dc9868513ea854eef7ae36f1c2ed8a578c7
patelpriya0987 <145538537+patelpriya0987@users.noreply.github.com>
2024-07-11 12:38:33 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
 let dogList = document.getElementById('dogList');
+let dogimg = document.getElementById('dog-img');
 const getList = () => {
 
     fetch('https://dog.ceo/api/breeds/list/all').then(res => {
@@ -14,11 +15,11 @@ const getList = () => {
             for(let breed in breedList){
                 if(breedList[breed].length == 0){
                     // console.log(breed);
-                    dogList.innerHTML += `<li>${breed}</li>`
+                    dogList.innerHTML += `<li style="cursor:pointer" onclick="return dogImage('${breed}')">${breed}</li>`
                 }else{
                     dogsub = "<ol>";
                     for(let subBreed in breedList[breed]){
-                        dogsub += `<li class="text-primary">${breedList[breed][subBreed]}</li>`
+                        dogsub += `<li class="text-primary" style="cursor:pointer" onclick="return dogImage('${breed}')">${breedList[breed][subBreed]}</li>`
                     }
                     dogsub += "</ol>";
 
@@ -35,3 +36,24 @@ const getList = () => {
 }
 
 getList();
+
+const dogImage = (breed) => {
+    fetch(`https://dog.ceo/api/breed/${breed}/images`).then(res => {
+        return res.json();
+    }).then(data => {
+        console.log(data);
+        let dogImage = data.message;
+        dogimg.innerHTML = "";
+        dogImage.forEach((img) => {
+            dogimg.innerHTML += `
+            <div class="col-3 p-3">
+                <div style="width: 400px; height: 400px;">
+                  <img id="" src="${img}" alt="img" class="w-100 h-100 object-fit-cover">
+                </div>
+              </div>`;            
+        })
+
+    }).catch(err => {
+        console.log(err);
+    });
+}
